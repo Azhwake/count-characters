e2b5cd273c9d9b7ef4219937108c595eec2cd12c
Azhwake <azhwaq2002@gmail.com>
2024-09-21 22:13:53 +0300

    done

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 // Good Luck 💪🏾
+function count(char, str) {
+    return str.split(char).length - 1;
+}
+
+console.log(count("a", "fikrcamp")); // ➞ 1
+console.log(count("c", "bootcamp")); // ➞ 1
+console.log(count("o", "fikrcamp coding bootcamp")); // ➞ 3
